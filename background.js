be018c775798d725b9c5f954efdfc62cc0c83ef7
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-03-24 13:04:52 +0100

    v2 with dl permission

--- background.js
+++ background.js
@@ -15,9 +15,9 @@ function getFilename()
 	ts = ts + ((t<10)?"0":"") + t + "_";
 	// time hh:mm:ss
 	t = (d.getHours()+1);
-	ts = ts + ((t<10)?"0":"") + t + ":";
+	ts = ts + ((t<10)?"0":"") + t + "-";
 	t = (d.getMinutes()+1);
-	ts = ts + ((t<10)?"0":"") + t + ":";
+	ts = ts + ((t<10)?"0":"") + t + "-";
 	t = (d.getSeconds()+1);
 	ts = ts + ((t<10)?"0":"") + t + "";
 	// filetype
@@ -25,15 +25,70 @@ function getFilename()
 	// 
 	return ts;
 }
+/*
+function requestPermissions(permissionsToRequest, granted, refused) {
+
+  function onResponse(response) {
+    if (response) {
+      console.log("Permission was granted");
+      granted(response);
+    } else {
+      console.log("Permission was refused");
+      refused(response);
+    }
+    return browser.permissions.getAll();
+  }
+
+  browser.permissions.request(permissionsToRequest)
+    .then(onResponse)
+    .then((currentPermissions) => {
+    console.log(`Current permissions:`, currentPermissions);
+  });
+}
+*/
+
+function dataURItoBlob(dataurl) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], {type:mime});
+}
+
+//**blob to dataURL**
+function blobToDataURL(blob, callback) {
+    var a = new FileReader();
+    a.onload = function(e) {callback(e.target.result);}
+    a.readAsDataURL(blob);
+}
 
 async function onBrowserActionClicked(tab) { 
+	//const url = new  URL(tab.url);
+	//console.log(url);
+	//const origin = url.origin;
+	//console.log(origin);
+	//requestPermissions({origins: [ origin + "/*" ]}, async function() {
 	const options = { format: "png" };
 	const dataURI = await browser.tabs.captureTab(tab.id,options);
-	await browser.tabs.executeScript({file: 'content-script.js'});
+/*	await browser.tabs.executeScript({file: 'content-script.js'});
 	await browser.tabs.sendMessage(tab.id, {
 		 "dataURI": dataURI 
 		,"filename": getFilename() 
 	});
+*/
+	//});
+/**/
+	const blob = dataURItoBlob(dataURI);
+	const filename = getFilename();
+	console.log('filename', filename);
+	const dl_options = {
+		 url: window.URL.createObjectURL(blob)
+		,filename: filename
+		,saveAs: true
+	};
+	await browser.downloads.download(dl_options);
+/**/
 }
 
 browser.browserAction.onClicked.addListener(onBrowserActionClicked); 
