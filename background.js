78af6c45957bdab2ef82b8b938a4eb309b9e7122
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-03-24 15:20:25 +0100

    feat: use browserAction popup for dl link

--- background.js
+++ background.js
@@ -1,8 +1,5 @@
 
-const extId = "Simple-Screenshot";
-
-function getFilename()
-{
+function getFilename() {
 	const d = new Date();
 
 	let ts = extId + "_";
@@ -25,70 +22,31 @@ function getFilename()
 	// 
 	return ts;
 }
-/*
-function requestPermissions(permissionsToRequest, granted, refused) {
 
-  function onResponse(response) {
-    if (response) {
-      console.log("Permission was granted");
-      granted(response);
-    } else {
-      console.log("Permission was refused");
-      refused(response);
-    }
-    return browser.permissions.getAll();
-  }
-
-  browser.permissions.request(permissionsToRequest)
-    .then(onResponse)
-    .then((currentPermissions) => {
-    console.log(`Current permissions:`, currentPermissions);
-  });
-}
-*/
-
-function dataURItoBlob(dataurl) {
-    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-    while(n--){
-        u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new Blob([u8arr], {type:mime});
-}
-
-//**blob to dataURL**
-function blobToDataURL(blob, callback) {
-    var a = new FileReader();
-    a.onload = function(e) {callback(e.target.result);}
-    a.readAsDataURL(blob);
-}
-
-async function onBrowserActionClicked(tab) { 
-	//const url = new  URL(tab.url);
-	//console.log(url);
-	//const origin = url.origin;
-	//console.log(origin);
-	//requestPermissions({origins: [ origin + "/*" ]}, async function() {
-	const options = { format: "png" };
-	const dataURI = await browser.tabs.captureTab(tab.id,options);
-/*	await browser.tabs.executeScript({file: 'content-script.js'});
-	await browser.tabs.sendMessage(tab.id, {
-		 "dataURI": dataURI 
-		,"filename": getFilename() 
+function simulateClick(elem) {
+	const evt = new MouseEvent('click', {
+		bubbles: false,
+		cancelable: false,
+		view: window
 	});
-*/
-	//});
-/**/
-	const blob = dataURItoBlob(dataURI);
-	const filename = getFilename();
-	console.log('filename', filename);
-	const dl_options = {
-		 url: window.URL.createObjectURL(blob)
-		,filename: filename
-		,saveAs: true
-	};
-	await browser.downloads.download(dl_options);
-/**/
+	elem.dispatchEvent(evt);
 }
 
-browser.browserAction.onClicked.addListener(onBrowserActionClicked); 
+const extId = "Simple-Screenshot";
+
+//let link = document.createElement('a');
+//link.style.display = 'none';
+//link.setAttribute('target', '_blank');
+//document.body.append(link);
+
+const options = { format: "png" };
+//const dataURI = 
+browser.tabs.captureTab(options).then(function(dataURI) {
+	const filename = getFilename();
+
+	let link = document.querySelector('#link');
+	link.setAttribute('download', filename);
+	link.setAttribute('href', dataURI);
+	simulateClick(link);
+});
+
