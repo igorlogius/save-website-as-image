13f926f69c3e617e6fa5b5def9f7c0feff8f7086
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-03-24 16:31:25 +0100

    refactor: cleanup background.js

--- background.js
+++ background.js
@@ -1,50 +1,29 @@
+//const options = { format: "png" };
+const extId = "Simple-Screenshot";
+const link = document.querySelector('#link');
 
 function getFilename() {
 	const d = new Date();
-
-	let ts = extId + "_";
-	// date YYYY-MM-DD
-	t = d.getFullYear();
-	ts = ts + t + "-";
-	t = (d.getMonth()+1);
-	ts = ts + ((t<10)?"0":"") + t + "-";
-	t = (d.getDate()+1);
-	ts = ts + ((t<10)?"0":"") + t + "_";
-	// time hh:mm:ss
-	t = (d.getHours()+1);
-	ts = ts + ((t<10)?"0":"") + t + "-";
-	t = (d.getMinutes()+1);
-	ts = ts + ((t<10)?"0":"") + t + "-";
-	t = (d.getSeconds()+1);
-	ts = ts + ((t<10)?"0":"") + t + "";
-	// filetype
-	ts = ts + ".png";
-	// 
+	let ts = extId;
+	// YYYY-MM-DD-hh-mm-ss
+	[d.getFullYear(), d.getMonth()+1, d.getDate()+1, 
+	    d.getHours(), d.getMinutes(), d.getSeconds()].forEach((t,i) => {
+		ts = ts + ((i!==3)?"-":"_") + ((t<10)?"0":"") + t;
+	});
 	return ts;
 }
 
 function simulateClick(elem) {
 	const evt = new MouseEvent('click', {
-		bubbles: false,
-		cancelable: false,
-		view: window
+		 bubbles: false
+		,cancelable: false
+		,view: window
 	});
 	elem.dispatchEvent(evt);
 }
 
-const extId = "Simple-Screenshot";
-
-//let link = document.createElement('a');
-//link.style.display = 'none';
-//link.setAttribute('target', '_blank');
-//document.body.append(link);
-
-const options = { format: "png" };
-//const dataURI = 
-browser.tabs.captureTab(options).then(function(dataURI) {
+browser.tabs.captureTab(/*options*/).then(function(dataURI) {
 	const filename = getFilename();
-
-	let link = document.querySelector('#link');
 	link.setAttribute('download', filename);
 	link.setAttribute('href', dataURI);
 	simulateClick(link);
