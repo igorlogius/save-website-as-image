7e530e54a84f446af169e70966306bc9bab10bc2
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-09-02 10:50:51 +0200

    feat: capture multiple selected tabs simultaniously

--- background.js
+++ background.js
@@ -19,47 +19,56 @@
 		return ts;
 	}
 
-	function simulateClick(elem) {
-		const evt = new MouseEvent('click', {
-			bubbles: false
-			,cancelable: false
-			,view: window
-		});
-		elem.dispatchEvent(evt);
-	}
+    const tabs = await browser.tabs.query({ highlighted: true, currentWindow: true });
 
-	try {
-		let tmp = await browser.tabs.executeScript({ code:
-`var body = document.body,html = document.documentElement;
-var h = Math.max( body.scrollHeight, body.offsetHeight,  html.clientHeight, html.scrollHeight, html.offsetHeight );
-var w = Math.max( body.scrollWidth, body.offsetWidth,  html.clientWidth, html.scrollWidth, html.offsetWidth );
-[w,h,document.title];`
-		});
+    let success = 0;
+    let tmp = '';
 
-		if (tmp.length < 1) {
-			throw 'Error: failed to get page width and height';
-		}
-		// executeScript returns an array with the first element being the result
-		tmp = tmp[0];
+    message.innerText = 'Failed to save:\n';
 
-		const dataURI = await browser.tabs.captureTab({
-			rect: {
-				x:0,
-				y:0,
-				width: tmp[0],
-				height: tmp[1]
-			}
-		});
-		let filename = getFilename();
-		if(tmp[2].length > 0){
-			filename = filename + " " + tmp[2];
-		}
-		link.setAttribute('download', filename);
-		link.setAttribute('href', dataURI);
-		simulateClick(link);
+    const tsFilename = getFilename();
 
-	}catch(e) {
-		message.innerText = extname + ' failed: \n(' + e.toString() + ")";
-	}
+    for(const tab of tabs) {
+        try {
+            //console.log(tab.id, tab.index);
+            //await browser.tabs.highlight({tabs: [tab.index]});
+            tmp = await browser.tabs.executeScript(tab.id, { code:
+    `var body = document.body,html = document.documentElement;
+    var h = Math.max( body.scrollHeight, body.offsetHeight,  html.clientHeight, html.scrollHeight, html.offsetHeight );
+    var w = Math.max( body.scrollWidth, body.offsetWidth,  html.clientWidth, html.scrollWidth, html.offsetWidth );
+    [w,h,document.title];`
+            });
+
+            if (tmp.length < 1) {
+                throw 'Error: failed to get page width and height';
+            }
+            // executeScript returns an array with the first element being the result
+            tmp = tmp[0];
+
+            const dataURI = await browser.tabs.captureTab(tab.id,{
+                rect: {
+                    x:0,
+                    y:0,
+                    width: tmp[0],
+                    height: tmp[1]
+                }
+            });
+            let filename = tsFilename + "_" + success;
+            if(tmp[2].length > 0){
+                filename = filename + " " + tmp[2];
+            }
+            link.setAttribute('download', filename);
+            link.setAttribute('href', dataURI);
+            link.click();
+            success++;
+
+        }catch(e) {
+            //message.innerText += extname + ' failed: \n(' + e.toString() + ")";
+            message.innerText +=  (" - " + tab.url + " (" + e.toString() + ")\n");
+        }
+    }
+    if(success === tabs.length){
+        message.innerText = 'Saved all selected';
+    }
 
 })();
