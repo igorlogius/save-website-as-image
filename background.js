ae75d86adbf2575381ccf5ffb2c033dc27e91227
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-05-12 12:03:39 +0200

    fix(#1): limit/reduce filenames

--- background.js
+++ background.js
@@ -4,118 +4,120 @@ const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
 function notify(title, message = "", iconUrl = "icon.png") {
-    return browser.notifications.create(""+Date.now(),
-        {
-           "type": "basic"
-            ,iconUrl
-            ,title
-            ,message
-        }
-    );
+  return browser.notifications.create("" + Date.now(), {
+    type: "basic",
+    iconUrl,
+    title,
+    message,
+  });
 }
 
 function getFilename() {
-	const d = new Date();
-	let ts = extname;
-	// YYYY-MM-DD-hh-mm-ss
-[d.getFullYear(), d.getMonth()+1, d.getDate()+1,
-    d.getHours(), d.getMinutes(), d.getSeconds()].forEach((t,i) => {
-    ts = ts + ((i!==3)?"-":" ") + ((t<10)?"0":"") + t;
-	});
-	return ts;
+  const d = new Date();
+  let ts = "SWAI";
+  // YYYY-MM-DD-hh-mm-ss
+  [
+    d.getFullYear(),
+    d.getMonth() + 1,
+    d.getDate() + 1,
+    d.getHours(),
+    d.getMinutes(),
+    d.getSeconds(),
+  ].forEach((t, i) => {
+    ts = ts + (i !== 3 ? "-" : " ") + (t < 10 ? "0" : "") + t;
+  });
+  return ts;
 }
 
 async function getFromStorage(type, id, fallback) {
-    let tmp = await browser.storage.local.get(id);
-    return (typeof tmp[id] === type) ? tmp[id] : fallback;
+  let tmp = await browser.storage.local.get(id);
+  return typeof tmp[id] === type ? tmp[id] : fallback;
 }
 
 async function onBAClicked() {
+  const tabs = (
+    await browser.tabs.query({ highlighted: true, currentWindow: true })
+  ).sort((a, b) => a.index - b.index);
 
-    const tabs = (await browser.tabs.query({ highlighted: true, currentWindow: true })).sort( (a,b) => (a.index - b.index));
+  if (tabs.length < 0) {
+    return;
+  }
+  notify(extname, "Capturing Images for " + tabs.length + " Tabs");
 
-    if(tabs.length < 0){
-	    return;
-    }
-    notify(extname, "Capturing Images for " + tabs.length + " Tabs");
+  const stepHeight = await getFromStorage("number", "stepHeight", 10000);
 
-    const stepHeight = await getFromStorage('number', 'stepHeight',10000);
+  let success = 0;
+  let tmp = "";
 
-    let success = 0;
-    let tmp = '';
+  let msgs = [];
+  const tsFilename = getFilename();
 
-    let msgs = [];
-    const tsFilename = getFilename();
-
-    for(const tab of tabs) {
-        try {
-            tmp = await browser.tabs.executeScript(tab.id, { code:
-    `var body = document.body,html = document.documentElement;
+  for (const tab of tabs) {
+    try {
+      tmp = await browser.tabs.executeScript(tab.id, {
+        code: `var body = document.body,html = document.documentElement;
     var h = Math.max( body.scrollHeight, body.offsetHeight,  html.clientHeight, html.scrollHeight, html.offsetHeight );
     var w = Math.max( body.scrollWidth, body.offsetWidth,  html.clientWidth, html.scrollWidth, html.offsetWidth );
-    [w,h,window.location.href, document.title];`
-            });
+    [w,h,window.location.href, document.title];`,
+      });
 
-            if (tmp.length < 1) {
-                throw 'Error: failed to get page width and height';
-            }
-            // executeScript returns an array with the first element being the result
-            tmp = tmp[0];
+      if (tmp.length < 1) {
+        throw "Error: failed to get page width and height";
+      }
+      // executeScript returns an array with the first element being the result
+      tmp = tmp[0];
 
-	    //console.debug(tmp[0],tmp[1]);
-	    
-		let dataURI;
+      //console.debug(tmp[0],tmp[1]);
 
-		// First Part (try to get entries page)
+      let dataURI;
 
-		let y_offset = 0;
-			let i=1;
+      // First Part (try to get entries page)
 
-		    while(tmp[1] > y_offset){
+      let y_offset = 0;
+      let i = 1;
 
-			    dataURI = await browser.tabs.captureTab(tab.id,{
-				format: "jpeg",
-				quality: 90,
-				rect: {
-				    x:0,
-				    y:y_offset,
-				    width: tmp[0],
-				    height: (tmp[1] > (y_offset+stepHeight))? stepHeight: (tmp[1]-y_offset)
-				}
-			    });
-			    y_offset = y_offset + stepHeight;
+      while (tmp[1] > y_offset) {
+        dataURI = await browser.tabs.captureTab(tab.id, {
+          format: "jpeg",
+          quality: 90,
+          rect: {
+            x: 0,
+            y: y_offset,
+            width: tmp[0],
+            height:
+              tmp[1] > y_offset + stepHeight ? stepHeight : tmp[1] - y_offset,
+          },
+        });
+        y_offset = y_offset + stepHeight;
 
-			    	    let filename = tsFilename + " Tab " + success + " Part " + (i) ;
-				    if(tmp[2].length > 0){
-					filename = filename + " " + tmp[2];
-				    }
-				    if(tmp[3].length > 0){
-					filename = filename + " " + tmp[3];
-				    }
-				    filename = filename.replaceAll('.','_');
-
-				    const link = document.createElement('a');
-				    link.setAttribute('download', filename);
-				    link.setAttribute('href', dataURI);
-				    link.click();
-			    	    link.remove();
-			    i++;
-		    }
-				
-	    
-	    success++;
-
-        }catch(e) {
-            msgs.push(' - Tab ' + tab.url  + ' (' + e.toString() + ')');
+        let filename = tsFilename + " Tab " + success + " Part " + i;
+        if (tmp[2].length > 0) {
+          filename = filename + " " + tmp[2].substr(8, 35);
         }
-    }
+        if (tmp[3].length > 0) {
+          filename = filename + " " + tmp[3].substr(0, 45);
+        }
+        filename = filename.replaceAll(".", "_");
 
-    if(success === tabs.length){
-    	notify(extname, "Completed");
-    }else{
-    	notify(extname, "Completed with errors:\n " + msgs.join("\n") );
+        const link = document.createElement("a");
+        link.setAttribute("download", filename);
+        link.setAttribute("href", dataURI);
+        link.click();
+        link.remove();
+        i++;
+      }
+
+      success++;
+    } catch (e) {
+      msgs.push(" - Tab " + tab.url + " (" + e.toString() + ")");
     }
+  }
+
+  if (success === tabs.length) {
+    notify(extname, "Completed");
+  } else {
+    notify(extname, "Completed with errors:\n " + msgs.join("\n"));
+  }
 }
 
 browser.browserAction.onClicked.addListener(onBAClicked);
-
--- options.js
+++ options.js
@@ -1,42 +1,39 @@
 /* global browser */
 
 function onChange(evt) {
+  let id = evt.target.id;
+  let el = document.getElementById(id);
 
-	let id = evt.target.id;
-	let el = document.getElementById(id);
+  let value = el.type === "checkbox" ? el.checked : el.value;
+  let obj = {};
 
-	let value = ( (el.type === 'checkbox') ? el.checked : el.value)
-	let obj = {}
+  //console.debug('value:', id,value, 'type:', el.type, 'min:', el.min, 'max', el.max);
 
-	//console.debug('value:', id,value, 'type:', el.type, 'min:', el.min, 'max', el.max);
+  if (value === "") {
+    return;
+  }
+  if (el.type === "number") {
+    try {
+      value = parseInt(value);
+      if (isNaN(value)) {
+        value = el.min;
+      }
+      if (value < el.min) {
+        value = el.min;
+      }
+    } catch (e) {
+      value = el.min;
+    }
+  }
 
-	if(value === ""){
-		return;
-	}
-	if(el.type === 'number'){
-		try {
-			value = parseInt(value);
-			if(isNaN(value)){
-				value = el.min;
-			}
-			if(value < el.min) {
-				value = el.min;
-			}
-		}catch(e){
-			value = el.min
-		}
-	}
+  obj[id] = value;
 
-	obj[id] = value;
-
-	//console.debug(id,value);
-	browser.storage.local.set(obj).catch(console.error);
-
+  //console.debug(id,value);
+  browser.storage.local.set(obj).catch(console.error);
 }
 
-function onLoad(){
-
-	/*
+function onLoad() {
+  /*
 	const manifest = browser.runtime.getManifest();
 	const toolbarActionSelect = document.getElementById('toolbarAction');
 	for(const cmd of Object.keys(manifest.commands)){
@@ -44,38 +41,35 @@ function onLoad(){
 	}
 	*/
 
-	[ "stepHeight"].map( (id) => {
+  ["stepHeight"].map((id) => {
+    browser.storage.local
+      .get(id)
+      .then((obj) => {
+        let el = document.getElementById(id);
+        let val = obj[id];
 
-		browser.storage.local.get(id).then( (obj) => {
+        if (typeof val !== "undefined") {
+          if (el.type === "checkbox") {
+            el.checked = val;
+          } else {
+            el.value = val;
+          }
+        } else {
+          el.value = (() => {
+            switch (id) {
+              case "stepHeight":
+                return 10000;
+              default:
+                return 0;
+            }
+          })();
+        }
+      })
+      .catch(console.error);
 
-			let el = document.getElementById(id);
-			let val = obj[id];
-
-			if(typeof val !== 'undefined') {
-				if(el.type === 'checkbox') {
-					el.checked = val;
-				}
-				else{
-					el.value = val;
-				}
-			}else{
-				el.value = (() => {
-					switch(id){
-						case "stepHeight":
-							return 10000;
-						default:
-							return 0;
-					}
-				})();
-			}
-
-		}).catch( console.error );
-
-		let el = document.getElementById(id);
-		el.addEventListener('input', onChange);
-	});
-
+    let el = document.getElementById(id);
+    el.addEventListener("input", onChange);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', onLoad);
-
+document.addEventListener("DOMContentLoaded", onLoad);
